25ebe06b95d6c6ba68acea78e6dbcabbd6fa94eb
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-10-21 14:52:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,16 +5,27 @@ import static java.lang.System.err;
 public class Main
 {
 
+    public static String hashpass="AC57D590ABE6D12D9A8EB691DB5975DE3677CAC168BF41C60D70DE4641681BE5";
+    public static String pass;
+    private static void initialize()
+    {
+        SubintervalSet SS=new SubintervalSet(10000000,99999999,121);
+        SS.start();
+    }
+
     public static void main(String[] args)
     {
-        try{
-            Thread[] thread = new Thread[8];
+        try {
+
+            initialize();
+            Thread[] thread = new Thread[121];
             for (int i = 0; i < thread.length; i++) {
-                thread[i] = new MyThread("Hello world from thread" + i);
+                thread[i] = new MyThread(i, IntervalsControl.mininterval[i], IntervalsControl.maxinterval[i]);
             }
             for (int i = 0; i < thread.length; i++) {
                 thread[i].start();
             }
+
         }catch (Exception e){
             System.out.println("Got an exception! " + err);
         }
