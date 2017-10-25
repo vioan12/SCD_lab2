5a683496824326d1dcc7e767511a1aae7316e096
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-10-25 21:11:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main
 {
 
     public static String hashpass="AC57D590ABE6D12D9A8EB691DB5975DE3677CAC168BF41C60D70DE4641681BE5";
-    public static String pass;
     private static void initialize()
     {
         SubintervalSet SS=new SubintervalSet(10000000,99999999,121);
@@ -25,6 +24,10 @@ public class Main
             for (int i = 0; i < thread.length; i++) {
                 thread[i].start();
             }
+            for(int i=0;i<thread.length;i++){
+                thread[i].join();
+            }
+            System.out.println("Parola " + MyThread.pass + " a fost gasita in Threadul " + MyThread.nr_thread);
 
         }catch (Exception e){
             System.out.println("Got an exception! " + err);
